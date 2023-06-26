9c49516dfcc3172d2aad53e20b23459f4690e361
Aayush Thapaliya <106351327+aayush6200@users.noreply.github.com>
2023-06-26 11:34:04 -0700

    Update Display.js

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -4,6 +4,8 @@ import './Display.css'
 
 const MyDisplay=(props)=>{
     return(
+
+        // displays the numbers on the screen 
         <div className='display-items'>
             <h1 value='0'>{props.data}</h1>
         </div>
